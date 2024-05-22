e931c34a0f4fc48d2c8deea3cb125c4e2add3333
Samuel Souza <samuel.souzato@gmail.com>
2024-05-22 15:56:54 -0400

    secun

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -49,7 +49,16 @@
             </nav>
         </header>
 
-        @yield('content')
+        <main>
+            <div class="container-fluid">
+                <div class="row">
+                    @if(session('msg'))
+                        <p class="msg">{{ session('msg') }}</p>
+                    @endif
+                    @yield('content') 
+                </div>
+            </div>
+        </main>
         <footer>
             <p>HDC Events &copy; 2024</p>
         </footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,12 +20,12 @@
         @foreach ($events as $event)
 
         <div class="card col-md-3">
-            <img src="/img/event_placeholder.jpg" alt="{{ $event->tilte }}">
+        <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
             <div class="card-boy">
                 <p class="card-date">10/09/2020</p>
                 <h5 class="card-title">{{ $event->title }}</h5>
                 <p class="card-participantes">X card-participantes</p>
-                <a href="#" class="btn btn-primary">Saber mais</a>
+                <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
             </div>
         </div>
         
